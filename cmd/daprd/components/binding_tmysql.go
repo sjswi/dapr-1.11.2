5682bd97cc135e62227d8c6d567beb0976b6b73b
yuxiaobing <yuxiaobing21s@ict.ac.cn>
2023-10-23 19:05:41 +0800

    tmysql 实现完成

---
+++ cmd/daprd/components/binding_tmysql.go
@@ -0,0 +1,10 @@
+package components
+
+import (
+	"github.com/dapr/components-contrib/bindings/tmysql"
+	bindingsLoader "github.com/dapr/dapr/pkg/components/bindings"
+)
+
+func init() {
+	bindingsLoader.DefaultRegistry.RegisterOutputBinding(tmysql.NewTMysql, "tmysql")
+}
